289c6c8e892f916aa1f34774e7818e11b7d59330
Marcin Zyla <marcin.zyla13@gmail.com>
2022-01-20 23:03:56 +0100

    basic concept implementation

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return render_template("MainPage.html")
+
+@app.route("/realizacje")
+def realizations():
+    return render_template("Realizations.html")
+
+@app.route("/omnie")
+def aboutMe():
+    return render_template("AboutMe.html")
+
+@app.route("/sklep")
+def shop():
+    return render_template("Shop.html")
+
+@app.route("/kontakt")
+def contactForm():
+    return render_template("MainPage.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
