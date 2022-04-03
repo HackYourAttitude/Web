e6883538b604b9f5b7e570047f715fc53f7a9616
marcinZyla13 <82803199+marcinZyla13@users.noreply.github.com>
2022-04-03 15:38:45 +0200

    contact form implementation

--- server.py
+++ server.py
@@ -1,31 +1,41 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def main_page():
     return render_template("MainPage.html")
 
+
 @app.route("/realizacje")
 def realizations():
     return render_template("Realizations.html")
+
 
 @app.route("/omnie")
 def aboutMe():
     return render_template("AboutMe.html")
 
+
 @app.route("/sklep")
 def shop():
     return render_template("Shop.html")
 
-@app.route("/kontakt")
-def contactForm():
+
+@app.route("/kontakt", methods=["GET"])
+def contactFormLoadPage():
     return render_template("ContactForm.html")
 
 
+@app.route("/kontakt", methods=["POST"])
+def contactFormCollectData():
+    userInputData = request.data
+
+    # zapisujemy dane do bazy danych
+
+    return jsonify({}), 200
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
--- static/ContactFormJS.js
+++ static/ContactFormJS.js
@@ -1,3 +1,68 @@
-window.onload= ()=>{
-    alert('dupa');
+window.onload  = function(){
+
+    document.getElementById('button').addEventListener('click',CollectContactForm)
+
+}
+
+function CollectContactForm()
+{
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let issue = document.getElementById('issue').value;
+    let question = document.getElementById('question').value;
+    if(validateUserInput(name,email,question))
+    {
+        let dataPackage = {
+            "Name": name,
+            "Email": email,
+            "Issue": issue,
+            "Question":question
+        }
+        SendUserInput(dataPackage);
+    }
+
+
+}
+
+function validateUserInput(name, email, question)
+{
+        if(name === "")
+        {
+            alert('Podaj imie !');
+            return false;
+        }
+        if(email === "")
+        {
+            alert('Podaj email !')
+            return false;
+        }
+        if(question === "")
+        {
+            alert('Zadaj pytanie !')
+            return false;
+        }
+        return true;
+
+}
+
+function SendUserInput(dataPackage)
+{
+    fetch("/kontakt", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataPackage)
+    }).then(response => response.status)
+        .then(data =>CheckResponseAndReact(data))
+
+}
+
+function CheckResponseAndReact(data)
+{
+    if(data === 200)
+        alert('Twój formularz został wysłany')
+    else
+        alert('Coś poszło nie tak, spróbuj ponownie ')
 }
